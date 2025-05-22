14ea620ce69fb75ebb6bdd3391be6db07b834e3d
Nazril Acil <nazrilacil04@gmail.com>
2025-05-22 16:28:12 +0700

    Create head.js

---
+++ src/app/head.js
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <title>SMK AL-MASTURIYAH LANGKAPLANCAR</title>
+      <meta name="description" content="Website resmi SMK AL-MASTURIYAH LANGKAPLANCAR" />
+      <meta name="google-site-verification" content="qNFq2NUCeYI5S87f8OUlDQq9HnzhmKj9e1jT8PTiGRA" />
+
+      {/* Open Graph & Twitter Card */}
+      <meta property="og:title" content="SMK AL-MASTURIYAH LANGKAPLANCAR" />
+      <meta property="og:description" content="Website SMK AL-MASTURIYAH LANGKAPLANCAR, PANGANDARAN" />
+      <meta property="og:image" content="https://smk-al-masturiyah.vercel.app/og-image.jpg" />
+      <meta property="og:url" content="https://smk-al-masturiyah.vercel.app" />
+      <meta name="twitter:card" content="summary_large_image" />
+    </>
+  );
+}
